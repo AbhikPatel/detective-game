c7b929959411e3c8a72f79ba4e3ff1bf9e388f20
AbhikPatel <abhikpatel15@gmail.com>
2022-07-25 23:51:49 +0530

    fix: bugs fixed in both missions

--- src/app/mission-two/mission-two.component.ts
+++ src/app/mission-two/mission-two.component.ts
@@ -13,6 +13,7 @@ export class MissionTwoComponent implements OnInit {
   public demo: boolean = false;
   public leave: boolean = false;
   public sheet: boolean = false;
+  public tutorial: boolean = false;
   public answerGroup!: FormGroup;
 
   constructor(private route: Router, private location: Location, private fb: FormBuilder) {
@@ -20,6 +21,12 @@ export class MissionTwoComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.sheet = true;
+    this.tutorial = true;
+    setTimeout(() => {
+      this.sheet = false;
+      this.tutorial = false;
+    }, 5000)
   }
 
   public prop() {
@@ -30,9 +37,6 @@ export class MissionTwoComponent implements OnInit {
     this.answerGroup = this.fb.group({
       ans: ['', [Validators.required]]
     })
-
-    console.log(this.route.url);
-
   }
 
   public onLeave() {
@@ -56,18 +60,25 @@ export class MissionTwoComponent implements OnInit {
   }
 
   public onCheck() {
-    if (this.answerGroup.value.ans === 'Hotel Indigo, berlin') {
+    let answer = this.answerGroup.value.ans.trim();
+    answer = answer.split(/[ ,]+/);
+
+    for (let i = 0; i < answer.length; i++) {
+      answer[i] = answer[i].charAt(0).toUpperCase() + answer[i].substr(1);
+    }
+
+    if (answer.toString() === 'Hotel,Indigo,Berlin') {
       this.demo = true;
       this.sheet = true;
       setTimeout(() => {
         this.sheet = false;
         this.demo = false;
         this.route.navigateByUrl('/landing');
-      }, 5000)
+      }, 3000)
     }
   }
 
-  public onNo(){
+  public onNo() {
     this.sheet = false;
     this.leave = false;
   }
